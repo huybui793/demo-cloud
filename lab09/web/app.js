e5fc89f3b1883e5fd799df29865e6401ffc415b0
huybui793 <huybtggch210911@fpt.edu.vn>
2023-06-22 10:01:13 +0700

    lab09

--- lab09/web/app.js
+++ lab09/web/app.js
@@ -54,7 +54,7 @@ app.use(function(err, req, res, next) {
 });
 
 //setup port for deployment
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 8000;
 app.listen(port); 
 
 module.exports = app;
